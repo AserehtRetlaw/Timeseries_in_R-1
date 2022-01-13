80126e4db75e2b0e216eb71e483bfa84e51dc059
AserehtRetlaw <theresa@walterbox.de>
2022-01-13 18:03:36 +0100

    ausgabe csv hourly

--- 2_scripts/Exercise_3.R
+++ 2_scripts/Exercise_3.R
@@ -7,11 +7,18 @@
 
 
 # Packages ----------------------------------------------------------------
-
-
+library(tidyverse)
+library(lubridate)
+library(dplyr)
 
 # Import Data -------------------------------------------------------------
+dwd_airport <- read.csv('https://raw.githubusercontent.com/AserehtRetlaw/Timeseries_in_R-1/main/1_data_processed/meteo_stations/dwd_airport.csv')
+dwd_urban <- read.csv('https://raw.githubusercontent.com/AserehtRetlaw/Timeseries_in_R-1/main/1_data_processed/meteo_stations/dwd_urban.csv')
+uni_meteo <- read.csv('https://raw.githubusercontent.com/AserehtRetlaw/Timeseries_in_R-1/main/1_data_processed/meteo_stations/uni_meteo.csv')
+wbi <- read.csv('https://raw.githubusercontent.com/AserehtRetlaw/Timeseries_in_R-1/main/1_data_processed/meteo_stations/wbi.csv')
+my_hobo <- read.csv('https://raw.githubusercontent.com/AserehtRetlaw/Timeseries_in_R-1/main/1_data_processed/10760710.csv')
 
 
+# Combine time and temperature values -------------------------------------
 
 
